dee11eba1ef0e58dbf2dd19a57b68b11311587bc
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-02-08 14:54:41 -0800

    connect to mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
 import bacteria from "./routes/bacteria"
+import connect from "./config/db"
 
 // Set environment variable
 dotenv.config()
+const { SERVER_PORT, MONGODB_URI } = process.env
+
+// Connect to database
+MONGODB_URI && connect(MONGODB_URI)
 
 // Setup server
 const app = express()
@@ -12,6 +17,6 @@ const app = express()
 app.use("/bacteria", bacteria)
 
 // listen to server
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`App listening on port: ${process.env.SERVER_PORT}`)
+app.listen(SERVER_PORT, () => {
+  console.log(`App listening on port: ${SERVER_PORT}`)
 })
