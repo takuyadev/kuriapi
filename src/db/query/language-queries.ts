afc85949ec304f6cf29c395ea86ce6a89fc7c13f
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-04-19 21:09:00 -0700

    Cleanup and comment languages queries

--- src/db/query/language-queries.ts
+++ src/db/query/language-queries.ts
@@ -1,16 +1,27 @@
 import db from "@/db/db";
 import { IsoCodeType } from "@/types/types.common";
+import { QueryResult } from "pg";
+
+// @desc Querries id of language with provided iso code
+// @params iso_code
 
 export const getIdByIsoCode = async (iso_code: IsoCodeType) => {
+
+   // Setup query for gettiing id
+   const query = `
+      SELECT id FROM languages
+      WHERE iso_code LIKE $1;
+   `;
+
    try {
-      const query = `
-         SELECT id FROM languages
-         WHERE iso_code LIKE $1;
-      `;
-      const res = await db.query(query, [iso_code]);
-      return res.rows[0].id as number;
+      // Attempt to request for id
+      const result: QueryResult = await db.query(query, [iso_code]);
+      const data: number = result.rows[0].id;
+
+      // Return id if success
+      return data
    } catch (err) {
-      return 2
+      // Return default of 2, for JP
+      return 2;
    }
 };
- 
